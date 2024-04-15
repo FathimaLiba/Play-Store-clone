5b64e940f5023e02ba250cf807f43585d600f419
unknown <libahamid2020@gmail.com>
2024-04-15 12:40:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+// ignore: camel_case_types
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Sample App",
+      home: GameScreen(),
+    );
+  }
+}
